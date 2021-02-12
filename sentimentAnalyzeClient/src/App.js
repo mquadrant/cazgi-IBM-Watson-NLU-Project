675d048bb01b862d4e5372a3b8b4cc0ab44ac175
mquadrant <mquadrant@gmail.com>
2021-02-12 18:47:03 +0000

    format sentiment on the frontend

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
     ret.then((response)=>{
 
       //Include code here to check the sentiment and fomrat the data accordingly
-
+        response.data = response.data.label
       this.setState({sentimentOutput:response.data});
       let output = response.data;
       if(response.data === "positive") {
